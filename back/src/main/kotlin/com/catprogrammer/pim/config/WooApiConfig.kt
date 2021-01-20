86b072f1c7ca52bcb8fb4e0bc1bdea0da0a52359
NathanKun <nathanhejunyang@gmail.com>
2021-01-21 00:21:48 +0200

    back - add WooApiConfig

---
+++ back/src/main/kotlin/com/catprogrammer/pim/config/WooApiConfig.kt
@@ -0,0 +1,11 @@
+package com.catprogrammer.pim.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "account.woo")
+data class WooApiConfig(
+    val key: String,
+    val secret: String
+)
