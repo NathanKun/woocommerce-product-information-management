7f309dd820a7eb657378fb11fe59fc0ef45f149d
NathanKun <nathanhejunyang@gmail.com>
2021-01-21 00:22:48 +0200

    front - when upload file to s3, change file name to random uuid

--- angular/src/app/service/s3.service.ts
+++ angular/src/app/service/s3.service.ts
@@ -5,6 +5,7 @@ import {Observable} from "rxjs";
 import {RestResponse} from "../interface/RestResponse";
 import {environment} from "../../environments/environment";
 import {map} from "rxjs/operators";
+import { v4 as uuidv4 } from 'uuid';
 
 
 @Injectable({providedIn: 'root'})
@@ -18,15 +19,18 @@ export class S3Service extends BaseHttpService {
     const formData = new FormData();
     const date = new Date()
     const path = `images/upload/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}/${date.getHours()}${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}`
+    const uuid = uuidv4()
+    const ext = file.name.split(".").pop()
+    const filename = uuid + "." + ext
 
     formData.append('file', file);
-    formData.append('filename', file.name);
+    formData.append('filename', filename);
     formData.append('path', path);
 
     return this.http.post<RestResponse<string>>(`${environment.api}/s3/`, formData).pipe(
       map(res => {
         if (res.success) {
-          return `https://woo-imgs.apeprogrammer.com/${path}/${file.name}`
+          return `https://woo-imgs.apeprogrammer.com/${path}/${filename}`
         } else {
           throw Error(res.data)
         }
