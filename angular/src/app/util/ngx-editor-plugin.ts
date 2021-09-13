0bf456627e1ded1f68468334d85cabc59c1bf5b9
NathanKun <nathanhejunyang@gmail.com>
2021-09-13 23:46:31 +0200

    front - ng update

--- angular/src/app/util/ngx-editor-plugin.ts
+++ angular/src/app/util/ngx-editor-plugin.ts
@@ -1,11 +1,5 @@
 import { Plugin } from 'prosemirror-state';
-import { image } from 'ngx-editor/plugins';
 
-const plugins: Plugin[] = [
-  image({
-    // enables image resizing
-    resize: true,
-  }),
-];
+const plugins: Plugin[] = [];
 
 export default plugins;
