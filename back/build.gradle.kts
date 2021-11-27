ac8757bcc01592f213690b15c343f2299ea204f7
NathanKun <nathanhejunyang@gmail.com>
2021-11-27 12:19:55 +0100

    back - upgrade dep

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    id("org.springframework.boot") version "2.5.4"
+    id("org.springframework.boot") version "2.5.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.github.ben-manes.versions") version "0.39.0" // dep update: gradlew dependencyUpdates -Drevision=release
 
@@ -42,11 +42,11 @@ dependencies {
 
     // implementation("de.codecentric:spring-boot-admin-starter-client:2.3.1") // spring boot admin client
 
-    val awsVersion = "1.12.70"
+    val awsVersion = "1.12.111"
     implementation("com.amazonaws:aws-java-sdk-core:${awsVersion}")
     implementation("com.amazonaws:aws-java-sdk-s3:${awsVersion}")
 
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.2")
 
     implementation(project(":angular"))
 }
